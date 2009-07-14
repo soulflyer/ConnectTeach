e268d32a53899d28b5fd7ccab44661d3bac89ef6
Iain Wood <public@soulflyer.co.uk>
2009-07-14 14:39:16 +0700

    About to start trying to add timetabling

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -4,4 +4,16 @@ class Tutor < ActiveRecord::Base
                     :url => ":class/:id/:style.:extension",
                     :path => ":rails_root/public/images/:class/:id/:style.:extension"
   has_one :User
+  validates_presence_of :given_name, :on => :create, :message => "can't be blank"
+  validates_presence_of :family_name, :on => :create, :message => "can't be blank"
+  validates_presence_of :sex, :on => :create, :message => "can't be blank"
+  validates_presence_of :given_name_first, :on => :create, :message => "can't be blank"
+  
+  def name
+    if given_name_first
+      given_name + " " + family_name
+    else
+      family_name + " " + given_name
+    end
+  end
 end
