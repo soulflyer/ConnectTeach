4a504be10658d7ae8826afdaab90089b0940ca6f
Iain Wood <public@soulflyer.co.uk>
2009-11-17 16:37:58 +0700

    More tidying

--- db/migrate/20091114215759_add_phone_and_address_to_tutor.rb
+++ db/migrate/20091114215759_add_phone_and_address_to_tutor.rb
@@ -1,11 +1,9 @@
 class AddPhoneAndAddressToTutor < ActiveRecord::Migration
   def self.up
     add_column :tutors, :address, :text
-    add_column :tutors, :phone, :integer
   end
 
   def self.down
     remove_column :tutors, :address
-    remove_column :tutors, :phone
   end
 end
