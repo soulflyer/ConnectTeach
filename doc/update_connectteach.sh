34b66e6c7aa7197dacff7f2a9c61da733cf78b6a
Iain Wood <public@soulflyer.co.uk>
2009-11-20 09:03:50 +0700

    Tweaked update script to kill the server and restart it automatically

--- doc/update_connectteach.sh
+++ doc/update_connectteach.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-echo "*****************************************"
-echo "Stop the mongrel server before continuing"
-echo "*****************************************"
-echo ""
+echo "***************************"
+echo "Stopping the mongrel server"
+echo "***************************"
+kill  `cat ~/public_html/connectteach/log/mongrel.pid`
 echo "The following files will be deleted by this script:"
 ls soulflyer-ConnectTeach*
 echo "Hit ctrl C now if this is a problem"
@@ -19,3 +19,6 @@ mv soulflyer-ConnectTeach* ~/public_html/connectteach
 echo "Now restarting the server"
 cd public_html/connectteach
 /usr/local/bin/ruby /usr/local/bin/mongrel_rails start -p 12002 -d -e production -P log/mongrel.pid
+echo "Now go to http://connectteach.com and check the bloody thing is still working"
+echo "Did you add a migration? If so go to public_html/connectteach and run:"
+echo "rake db:migrate RAILS_ENV=production"
