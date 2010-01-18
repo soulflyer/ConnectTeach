a3642ef525332f1bf33b73d4cb0cd9cad30f9916
Iain Wood <public@soulflyer.co.uk>
2010-01-18 17:48:43 +0700

    Fixed bug in picture index

--- doc/update_connectteach.sh
+++ doc/update_connectteach.sh
@@ -25,9 +25,11 @@ cd ~/public_html/connectteach/public/images/
 rm -r newsitems
 rm -r tutors
 rm -r flavours
+rm -r pictures
 ln -s ~/public_html/connectteach_images/newsitems newsitems
 ln -s ~/public_html/connectteach_images/tutors tutors
 ln -s ~/public_html/connectteach_images/flavours flavours
+ln -s ~/public_html/connectteach_images/pictures pictures
 echo "Now restart the server"
 # cd ~/public_html/connectteach
 # /usr/bin/ruby /usr/bin/mongrel_rails start -p 12002 -d -e production -P log/mongrel.pid
