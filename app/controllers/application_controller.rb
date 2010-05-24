d1d9a95fb0e49d5976348461d841c0463a047284
Iain Wood <public@soulflyer.co.uk>
2010-05-24 15:30:36 +0700

    Added I18n switching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include Clearance::Authentication
   include ExceptionNotifiable
   
+  before_filter :set_locale
   local_addresses.clear # always send email notifications instead of displaying the error
   
   helper :all # include all helpers, all the time
@@ -12,4 +13,14 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
+  def set_locale
+    I18n.locale = params[:locale]
+  end
+  
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+    # logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    
+  end
 end
